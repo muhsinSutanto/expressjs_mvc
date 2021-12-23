c2dd8c93bb4bd0b0a97dc5351a289d3d152587ce
muhsin sutanto <sutantomuhsin@gmail.com>
2021-12-23 10:08:38 +0700

    middleware, server, route, path, html

---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res) => {
+   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+
+router.post("/product", (req, res) => {
+   console.log(req.body);
+   res.redirect("/");
+});
+
+module.exports = router;
