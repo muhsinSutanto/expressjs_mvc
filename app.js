63bd968b8dbb7d42359819ff22363dc13cfa529b
muhsin sutanto <sutantomuhsin@gmail.com>
2021-12-23 10:49:14 +0700

    add static files config on app.js

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-   res.status(404).sendFile(path.join(__dirname, "views", "notfound.html"));
+   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 const server = http.createServer(app);
