f25f623bf30623e37f65aa89982f3343967e6e49
muhsin sutanto <sutantomuhsin@gmail.com>
2022-01-02 01:21:50 +0700

    setup ejs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -15,7 +17,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+   res.status(404).render("404", { pageTitle: "page not found" });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-   res.sendFile(path.join(rootDir, "views", "add-product.html"));
+   res.render("add-product", {
+      pageTitle: "add-product",
+      path: "/admin/add-product",
+      formCss: true,
+      productCss: true,
+      activeAddProduct: true,
+   });
 });
 
 // /admin/add-product => POST
