2a35e83abf313caa7039d2697403dbd6b97a85b3
muhsin sutanto <sutantomuhsin@gmail.com>
2022-01-12 04:09:41 +0700

    set model with static data

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
-app.set("view engine", "ejs");
-app.set("views", "views");
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+const app = express();
+//setting template engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//setting receiving body payload
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//setting static page
 app.use(express.static(path.join(__dirname, "public")));
 
+//setting routing
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
+//setting undifend page
 app.use((req, res, next) => {
    res.status(404).render("404", { pageTitle: "page not found", path: "eror" });
 });
